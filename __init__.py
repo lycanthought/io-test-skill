ef0b89d2e454c01b0f4617831d3994c224be483e
Dan Perrett <dan_perrett@hotmail.com>
2022-03-09 09:06:55 -0600

    message queue interaction updated

--- __init__.py
+++ __init__.py
@@ -4,16 +4,12 @@ import sysv_ipc as sysv
 
 message = 'test'
 message_type = 1
-key = 0
-queue = 0
+key = sysv.ftok("/home/robot/ftok_file", 1, True)
+queue = sysv.MessageQueue(key, sysv.IPC_CREAT)
 
 class IoTest(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    def initialize(self):
-        key = sysv.ftok("/home/robot/ftok_file", 1, True)
-        queue = sysv.MessageQueue(message_queue_key, sysv.IPC_CREAT)
 
     @intent_file_handler('test.io.intent')
     def handle_test_io(self, message):
