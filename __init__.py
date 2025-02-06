8cba21298a571c9099b0b4089d6808daecfee8d0
Dan Perrett <dan_perrett@hotmail.com>
2025-02-06 00:13:28 +0000

    message queue removed

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-import sysv_ipc as sysv
+#import sysv_ipc as sysv
 
 message_name_1 = "test"
 message_name_2 = "wake"
 message_type = 1
-key = sysv.ftok("/home/robot/ftok_file", 1, True)
-queue = sysv.MessageQueue(key, sysv.IPC_CREAT)
+#key = sysv.ftok("/home/robot/ftok_file", 1, True)
+#queue = sysv.MessageQueue(key, sysv.IPC_CREAT)
 
 class IoTest(MycroftSkill):
     def __init__(self):
@@ -19,10 +19,10 @@ class IoTest(MycroftSkill):
     @intent_file_handler('test.io.intent')
     def handle_test_io(self, message):
         self.speak_dialog('test.io')
-        queue.send(message_name_1, True, message_type)
+        #queue.send(message_name_1, True, message_type)
 
     def handle_wakeword(self, message):
-        queue.send(message_name_2, True, message_type)
+        #queue.send(message_name_2, True, message_type)
 
 
 def create_skill():
